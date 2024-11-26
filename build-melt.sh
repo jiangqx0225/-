f4744aecf86735bf59b6cb119c64cd9359bbf199
Jiang Qixiong <60382244+jiangqx0225@users.noreply.github.com>
2024-11-26 22:48:20 +0800

    Update build-melt.sh

--- build-melt.sh
+++ build-melt.sh
@@ -364,7 +364,7 @@ function set_globals {
   REPOLOCS[1]="https://github.com/mltframework/mlt.git"
   REPOLOCS[2]="https://github.com/dyne/frei0r.git"
   REPOLOCS[3]="https://github.com/mirror/x264.git"
-  REPOLOCS[4]="hhttps://github.com/webmproject/libvpx.git"
+  REPOLOCS[4]="https://github.com/webmproject/libvpx.git"
   REPOLOCS[5]="https://github.com/mltframework/swfdec.git"
   REPOLOCS[6]="https://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz?ts=gAAAAABnRdQeLrGV0hXdlxzvrSk92CSiqQ24648zeeQF7O0FiasTDvUnLwOQgS1Bc_ZCjOmAHa4fPihrgX0z8XSi-z2YHDOVEg%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Flame%2Ffiles%2Flame%2F3.99%2Flame-3.99.5.tar.gz%2Fdownload"
   REPOLOCS[7]="https://github.com/georgmartius/vid.stab.git"
